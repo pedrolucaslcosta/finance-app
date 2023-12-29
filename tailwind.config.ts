c46883408770656d0135a7085879ec8bc055c904
Pedro Lucas <pedrolucaslcosta@gmail.com>
2023-12-28 21:16:57 -0300

    Initial commit from Create Next App

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,7 +5,9 @@ const config: Config = {
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './node_modules/preline/preline.js',
   ],
+  darkMode: 'class',
   theme: {
     extend: {
       backgroundImage: {
@@ -15,6 +17,8 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
+  plugins: [
+    require('preline/plugin'),
+  ],
 }
 export default config
