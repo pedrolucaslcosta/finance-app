4bb7dc8a10e510b324f42428518a65fb58f2ee9b
Pedro Lucas <pedrolucaslcosta@gmail.com>
2024-12-18 15:09:55 -0300

    reset - go to ionic

--- resources/js/types/FixedExpense.ts
+++
@@ -1,9 +0,0 @@
-export interface FixedExpense {
-    id: number;
-    description: string;
-    billing_day: number;
-    amount: string;
-    status: 'pending' | 'paid';
-    created_at: string;
-    updated_at: string;
-}
